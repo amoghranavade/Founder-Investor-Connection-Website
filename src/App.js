864ee9724eafc8037dd51a9f36bcc9ee627800df
Amogh Ranavade <amogh.ranavade@yahoo.com>
2022-09-26 22:19:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GrowthCAP <code>src/App.js</code> and save to reload.
+          GrowthCAP <code>- founder and investor connection</code>
         </p>
         <a
           className="App-link"
