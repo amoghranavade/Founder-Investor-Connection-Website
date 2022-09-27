24f8bcf675bfa996e7c6b118e2214ab1ba830c4c
siddhi0409 <89137190+siddhi0409@users.noreply.github.com>
2022-09-27 11:28:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GrowthCAP <code>- founder and investor connection</code>
+          GrowthCAP Siddhi <code>- founder and investor connection</code>
         </p>
         <a
           className="App-link"
