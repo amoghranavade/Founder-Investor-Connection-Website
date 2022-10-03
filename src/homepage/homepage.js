120314fca339913f56555efe301e075f89399160
Amogh Ranavade <amogh.ranavade@yahoo.com>
2022-10-03 22:25:42 +0530

    React Router
    Made a router in app.js to be able to route through different pages.

---
+++ src/homepage/homepage.js
@@ -0,0 +1,24 @@
+import logo from '../logo.svg';
+import './homepage.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          GrowthCAP Siddhi <code>- founder and investor connection</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+learn react ria        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
