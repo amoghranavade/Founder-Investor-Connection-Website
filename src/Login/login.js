70e91c0663f8d05b2200f2ef12a667edadd00d20
Amogh Ranavade <amogh.ranavade@yahoo.com>
2022-10-08 22:26:04 +0530

    Perfect Design Update

--- src/Login/login.js
+++ src/Login/login.js
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 // import Homepage from "../Homepage/homepage";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { Button, Icon, Rating, Input } from 'semantic-ui-react';
+import { Label, Button, Icon, Rating, Input } from 'semantic-ui-react';
 import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -32,47 +32,34 @@ function Login() {
     
       <header className="Login-header">
       <div className="Card">
-        <p>
+        <p style={{fontSize: '30px'}}>
           GrowthCAP - Login
         </p>
         {/* <text style={{fontSize: 16, alignItems: 'top'}}>Username</text> */}
-        {/* <Input style={{width: "300px", height: '40px', fontSize: 16}}  placeholder='username' />
+        {/* <Label basic color='green' pointing='below'>
+        Please enter a value
+      </Label> */}
+   
+       
+   
+        <Input style={{width:'80%', fontSize:'18px'}} icon='users' iconPosition='left' placeholder='Username' />
         <br/>
-        <Input style={{width: "300px", height: '40px', fontSize: 16}}  placeholder='password' type='password' />
-        <br/> */}
+        <Input style={{width: "80%", fontSize: '18px'}} icon='key' iconPosition='left' placeholder='password' type='password' />
+        <br/>
       
-      <TextField id="outlined-basic" label="Username" variant="outlined" />
+      
        <br/>
-       <br/>
-        <TextField
-          style={{
-            backgroundColor: "white",
-            width: '60%',
-            borderRadius: '5%'
-        }}
-        InputProps={{
-            style: {
-                color: "black",
-                fontSize: '15px '
-            }
-        }}
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-       <br/>
-       <br/>
+      
 
-        <Button style={{width: "420px", height: '40px', backgroundColor: '#238636', color : '#FFF'}}  animated onClick={navigateToHomepage}>
+        <Button style={{width: "80%", backgroundColor: '#238636', color : '#FFF'}}  animated onClick={navigateToHomepage}>
           <Button.Content style={{fontSize: 18}}  visible>Login</Button.Content>
            <Button.Content hidden>
             <Icon name='arrow right' />
           </Button.Content>
         </Button>
         <br/>
-        <text style={{fontSize: 17}}>New here?<Link to="/register">Register now!</Link></text>
-
+        <text style={{fontSize: 17}}>New here?<Link to="/register"> Register now!</Link></text>
+        {/* lineHeight: 30 */}
        
         </div>
       </header>
--- src/Register/register.js
+++ src/Register/register.js
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 // import Homepage from "../Homepage/homepage";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { Button, Form, Checkbox, Icon, Rating, Input } from 'semantic-ui-react';
+import { Button,Label, Icon, Input } from 'semantic-ui-react';
 import React, { useEffect } from 'react';
 
 
@@ -26,26 +26,42 @@ function Register() {
  
   return (
     
-    <div className="Login">
+    <div className="Register">
     
-      <header className="Login-header">
+      <header className="Register-header">
       <div className="Card">
+        <br/>
+        <br/>
         <p>
           GrowthCAP - Register
         </p>
-        <text style={{fontSize: 16, alignItems: 'top'}}>Username</text>
+        {/* <text style={{fontSize: 16, alignItems: 'top'}}>Username</text>
         <Input style={{width: "300px", height: '40px', fontSize: 16}}  placeholder='username' />
+       
         <br/>
         <Input style={{width: "300px", height: '40px', fontSize: 16}}  placeholder='password' type='password' />
+        <br/> */}
+     
+        <Input style={{width:'80%', fontSize:'18px'}} icon='users' iconPosition='left' placeholder='Email' />
         <br/>
-        <Button style={{width: "300px", height: '40px',backgroundColor: '#238636', color : '#FFF'}}  animated>
+        <Input style={{width:'80%', fontSize:'18px'}} icon='users' iconPosition='left' placeholder='Username' />
+        <br/>
+        <Input style={{width: "80%", fontSize: '18px'}} icon='key' iconPosition='left' placeholder='password' type='password' />
+        <br/>
+        <Input style={{width: "80%", fontSize: '18px'}} icon='key' iconPosition='left' placeholder='re-enter password' type='password' />
+        <br/>
+        <br/>
+        <Button style={{width: "80%", height: '40px',backgroundColor: '#238636', color : '#FFF'}}  animated>
           <Button.Content style={{fontSize: 18}}  visible>Register</Button.Content>
            <Button.Content hidden>
             <Icon name='arrow right' />
           </Button.Content>
         </Button>
         <br/>
-        <text style={{fontSize: 17}}>Already a member?<Link to="/login">Login now!</Link></text>
+        <text style={{fontSize: 17}}>Already a member?<Link to="/login"> Register now!</Link></text>
+        <br/>
+        <br/>
+        
 
        
         </div>
