0432ce8df79ec710c5168f4d0bf4ac08a70ebc57
Amogh Ranavade <amogh.ranavade@yahoo.com>
2023-03-20 23:32:10 +0530

    20th March PUSH
    MAJOR PUSH

--- src/Components/HomepageGuest/homepageg.js
+++ src/Components/HomepageGuest/homepageg.js
@@ -34,61 +34,61 @@ const HomepageGuest = () => {
 
 
 
- const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  
- function runTextEffect() {
-   const h1 = document.querySelector("h1");
-   h1.dataset.value = h1.innerText;
+//  function runTextEffect() {
+//    const h1 = document.querySelector("h1");
+//    h1.dataset.value = h1.innerText;
    
-   let iteration = 0;
-   let interval = setInterval(() => {
-     h1.innerText = h1.innerText
-       .split("")
-       .map((letter, index) => {
-         if(index < iteration) {
-           return h1.dataset.value[index];
-         }
-         return letters[Math.floor(Math.random() * 26)]
-       })
-       .join("");
+//    let iteration = 0;
+//    let interval = setInterval(() => {
+//      h1.innerText = h1.innerText
+//        .split("")
+//        .map((letter, index) => {
+//          if(index < iteration) {
+//            return h1.dataset.value[index];
+//          }
+//          return letters[Math.floor(Math.random() * 26)]
+//        })
+//        .join("");
      
-     if(iteration >= h1.dataset.value.length){ 
-       clearInterval(interval);
-     }
+//      if(iteration >= h1.dataset.value.length){ 
+//        clearInterval(interval);
+//      }
      
-     iteration += 1 / 3;
-   }, 50);
- }
+//      iteration += 1 / 3;
+//    }, 50);
+//  }
  
- window.onload = () => {
-   runTextEffect();
- };
+//  window.onload = () => {
+//    runTextEffect();
+//  };
 
- const [showSecondBody, setShowSecondBody] = useState(true);
- const [showThirdBody, setShowThirdBody] = useState(true);
- const [hideFirstBody, setHideFirstBody] = useState(true);
+//  const [showSecondBody, setShowSecondBody] = useState(true);
+//  const [showThirdBody, setShowThirdBody] = useState(true);
+//  const [hideFirstBody, setHideFirstBody] = useState(true);
 
- useEffect(() => {
+//  useEffect(() => {
  
-   const timeoutId = setTimeout(() => {
-     setHideFirstBody(false);
+//    const timeoutId = setTimeout(() => {
+//      setHideFirstBody(false);
      
-    //  setHideFlyer(false);
-   }, 2000);
 
-   const timeoutSecond = setTimeout(() => {
-    setShowThirdBody(false);
-    setShowSecondBody(false);
-   //  setHideFlyer(false);
-  }, 2000);
+//    }, 2000);
+
+//    const timeoutSecond = setTimeout(() => {
+//     setShowThirdBody(false);
+//     setShowSecondBody(false);
+ 
+//   }, 2000);
 
 
 
-   return () => {
-    clearTimeout(timeoutSecond);
-    clearTimeout(timeoutId);
-   };
- }, []);
+//    return () => {
+//     clearTimeout(timeoutSecond);
+//     clearTimeout(timeoutId);
+//    };
+//  }, []);
  
 
   return (
@@ -99,34 +99,28 @@ const HomepageGuest = () => {
 
 
 
-    {/* {hideFirstFlyer && ( */}
-
-    
-    {/* )} */}
     
     <div className='mobileView' >
 
-    <div className={`firstBody ${hideFirstBody ? '' : 'hidden'}`}>
+    {/* <div className={`firstBody ${hideFirstBody ? '' : 'hidden'}`}>
         <h1 data-value="GrowthCAP">GrowthCAP</h1>
    
-    </div>
+    </div> */}
 
-    <div className={`secondBody ${showSecondBody ? '' : 'appear'}`}>
+    {/* <div className={`secondBody ${showSecondBody ? '' : 'appear'}`}> */}
     
+    
+    <div className='secondBody'>
       <div className='navbarMobileView'> {/*First Layout*/}
-          <img  style={{ height:'50px', width:'40px', marginTop:'4%', marginLeft:'2%'}} src={mainLogo}  alt="GrowthCAP-logo"/>
+          <img  style={{ height:'50px', width:'40px', marginTop:'2%', marginLeft:'2%'}} src={mainLogo}  alt="GrowthCAP-logo"/>
             
             <p className='growthCapNavText'>GrowthCAP</p>
-            {/* <h1 data-value="GrowthCAP">GrowthCAP</h1> */}
+            
             <Menu
             sx={{fontSize:'30px', marginTop:'5%', marginLeft:'23%'}}
             onClick={openNavMenu}
             />
-
-        
-      
-
-      </div>
+    </div>
       
 
       <div className='secondLayout' onClick={closeNavMenu}>
@@ -134,18 +128,23 @@ const HomepageGuest = () => {
           startup founders & investors.
         </p>
         <button onClick={goToLogin} className='toLoginButton'> Let's Go</button> 
-
+        <p className='viewDocText'>View documentation</p>
       </div>
+
+
       <div className='imageDivGuest' style={{ position: 'relative', marginTop: '-50px' }}>
-        <img style={{ marginLeft:'1%',position: 'absolute', top: '-20px', left: '0', height:'150px', width:'112px', borderRadius:'6px', marginTop: '-20px' }} src={imageOne}  alt="GrowthCAP-logo"/>
-        <img style={{ position: 'absolute', top: '-80px', left: '46%', transform: 'translateX(-40%)', height:'225px', width:'150px', borderRadius:'6px' }} src={imageTwo}  alt="GrowthCAP-logo"/>
-        <img style={{ marginLeft:'70%',position: 'absolute', top: '-20px', left: '0', height:'150px', width:'112px', borderRadius:'6px', marginTop: '-20px' }} src={imageThree}  alt="GrowthCAP-logo"/>
+        <img style={{ marginLeft:'1%',position: 'absolute', top: '-20px', left: '0', height:'150px', width:'112px', borderRadius:'6px', marginTop: '-20px' }} src={imageOne}  alt="investor-image"/>
+        <img style={{ position: 'absolute', top: '-80px', left: '46%', transform: 'translateX(-40%)', height:'225px', width:'150px', borderRadius:'6px' }} src={imageTwo}  alt="growthCAP-image"/>
+        <img style={{ marginLeft:'70%',position: 'absolute', top: '-20px', left: '0', height:'150px', width:'112px', borderRadius:'6px', marginTop: '-20px' }} src={imageThree}  alt="founder-image"/>
       </div>
 
     </div>
 
-    <div className={`thirdBody ${showThirdBody ? '' : 'appear'}`}>
-      <p className='headerOne'>Easy</p>
+    {/* <div className={`thirdBody ${showThirdBody ? '' : 'appear'}`}> */}
+
+
+    <div className='thirdBody'>
+      <p className='headerOne'>Easy Connections</p>
       <p className='headerTwo'>Fast</p>
     </div>
 
